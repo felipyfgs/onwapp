e09a6490dd4e184dc92481dd379f6d721354b8b2
Felipe Galvão <felipyfgs17@gmail.com>
2025-11-22 03:53:15 +0100

    feat: add Prisma module and service, update dependencies
    - Added PrismaModule and PrismaService for database interactions.
    - Updated app.module.ts to include PrismaModule.
    - Added new dependencies: pino for logging, qrcode-terminal for QR code generation, and whaileys for additional functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from './logger/logger.module';
 import { SessionModule } from './modules/session/session.module';
 import { MessageModule } from './modules/message/message.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [LoggerModule, SessionModule, MessageModule],
+  imports: [LoggerModule, SessionModule, MessageModule, PrismaModule],
   controllers: [],
   providers: [],
 })
